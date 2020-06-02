f399de4dd350ef51fc0d8a6932202e4d372b731b
Aditya Balasubramanyam <adbalasu@cisco.com>
2020-06-02 13:48:42 +0530

    Adding UserEntity

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -0,0 +1,199 @@
+package com.upgrad.quora.service.entity;
+
+
+import org.apache.commons.lang3.builder.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="users", schema = "quora")
+public class UserEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    private int uuid;
+
+    @Column(name = "FIRST_NAME")
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    @NotNull
+    @Size(max = 30)
+    private String lastName;
+
+    @Column(name = "USERNAME")
+    @NotNull
+    @Size(max = 30)
+    private String username;
+
+    @Column(name = "EMAIL")
+    @NotNull
+    @Size(max = 50)
+    private String email;
+
+    @ToStringExclude
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "SALT")
+    @NotNull
+    @Size(max = 200)
+    @ToStringExclude
+    private String salt;
+
+    @Column(name = "COUNTRY")
+    @NotNull
+    @Size(max = 30)
+    private String country;
+
+    @Column(name = "ABOUT_ME")
+    @NotNull
+    @Size(max = 50)
+    private String aboutMe;
+
+    @Column
+    @NotNull
+    @Size(max = 30)
+    private Date dob;
+
+    @Column(name = "ROLE")
+    @NotNull
+    @Size(max = 30)
+    private String role;
+
+    @Column(name = "CONTACT_NUMBER")
+    @NotNull
+    @Size(max = 30)
+    private String contactNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
