1012c609450bd9c33f43696626e65e02fd7804b4
urmilaunni88 <40367164+urmilaunni88@users.noreply.github.com>
2020-06-08 19:49:45 +0530

    Removed duplicated method

--- quora-service/src/main/java/com/upgrad/quora/service/business/AuthenticationService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AuthenticationService.java
@@ -17,39 +17,13 @@ import java.util.UUID;
 public class AuthenticationService {
 
 
-    @Autowired
-    private UserDao userDao;
+   @Autowired
+   private UserDao userDao;
 
-    @Autowired
-    private PasswordCryptographyProvider cryptographyProvider;
+   @Autowired
+   private PasswordCryptographyProvider cryptographyProvider;
 
-    //Sign In Validation
-    @Transactional(propagation = Propagation.REQUIRED)
-    public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
-        UserEntity userEntity = userDao.getUserByUsername(username);
-        System.out.println("INSIDE AUTHENTICATE USERNAME IS:"+username);
-        if (userEntity == null) {
-            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
-        }
-
-        final String encryptedPassword = cryptographyProvider.encrypt(password, userEntity.getSalt());
-        if (encryptedPassword.equals(userEntity.getPassword())) {
-            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
-            UserAuthTokenEntity userAuthToken = new UserAuthTokenEntity();
-            userAuthToken.setUser(userEntity);
-            final ZonedDateTime now = ZonedDateTime.now();
-            final ZonedDateTime expiresAt = now.plusHours(8);
-            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
-            userAuthToken.setLoginAt(now);
-            userAuthToken.setExpiresAt(expiresAt);
-            userAuthToken.setUuid(UUID.randomUUID().toString());
-            userDao.createAuthToken(userAuthToken);
-            userDao.updateUser(userEntity);
-            return userAuthToken;
-        } else {
-            throw new AuthenticationFailedException("ATH-002", "Password Failed");
-        }
-
+  
   /**
    * Method to validate signin
    *
